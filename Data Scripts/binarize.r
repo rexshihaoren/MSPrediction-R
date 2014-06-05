4ab80ebdb341ea10052f74dbf86c4647f6dd77cb
rexshihaoren <renshihao@gmail.com>
2014-06-05 02:34:42 -0700

    Fitting with poission, beta, and nbinom

--- Data Scripts/binarize.r
+++ Data Scripts/binarize.r
@@ -25,6 +25,48 @@ modfam2<-modfam2[,9:13]
 # get rid visitID
 fam2<-fam2[,-1]
 
+
+Binarize <- function(df, target){
+  # to Binarize a target column of a datafram
+  med <- median(df[[target]])
+  bin <- df
+  bin[[target]] <- ifelse(bin[[target]] <= med, 0,1)
+  return(bin)
+}
+
+modfam2_bin <-Binarize(modfam2, "EnjoyLife")
+fam2_bin <- Binarize(fam2, "EnjoyLife")
+
+
+
+DataProcessing <- function(df, target = ""){
+  # Processing a data frame to make everything (0, 1] except the target col
+  if (target == ""){
+    loc <- ncol(df)+1
+  } else{
+    loc <- grep(target, colnames(df))
+  }
+  processing <- df
+  processing <- apply(processing, 2, function(x) (x-min(x))/max(x)-min(x))
+  processing[,-loc] <- apply(processing[,-loc], c(1,2), function(x) if (x == 0) x+1e-12 else x)
+
+  as.data.frame(processing)
+}
+modfam2_processing <- DataProcessing(modfam2_bin, "EnjoyLife")
+fam2_processing <- DataProcessing(fam2_bin, "EnjoyLife")
+
+#save fam2, modfam2, fam2_bin, modfam2_bin, modfam2_processing, fam2_processing, and  merged in HDF5 format
+h5createFile('data/predData.h5')
+h5write(fam2, "data/predData.h5","fam2")
+h5write(modfam2,"data/predData.h5","modfam2")
+h5write(merged, "data/predData.h5", "merged")
+h5write(fam2_bin, "data/predData.h5","fam2_bin")
+h5write(modfam2_bin, "data/predData.h5","modfam2_bin")
+h5write(modfam2_processing, "data/predData.h5","modfam2_processing")
+h5write(fam2_processing, "data/predData.h5","fam2_processing")
+
+
+
 gendist<-function(somedf, plotfunc, target, filename){
   # Plotting distribution (histogram/density) given a dataframe and a target column, and a filename
   # Args:
@@ -41,29 +83,11 @@ gendist<-function(somedf, plotfunc, target, filename){
   # ggsave(file="plots/modfam2.pdf")
   ggsave(file=filepath)
 }
-gendist(modfam2, geom_histogram, "EnjoyLife", "modfam2")
-gendist(fam2, geom_histogram, "EnjoyLife", "fam2")
-gendist(fullTable3, geom_histogram, "ActualEDSS", "fullTable3")
-gendist(fullTable3, geom_density, "ActualEDSS", "fullTable3")
 
-# Binarize EnjoyLife
-# find the enjoy life median
-medianEL<- median(modfam2$EnjoyLife)
-# binarize the modfam2, fam2
-modfam2$EnjoyLife<-ifelse(modfam2$EnjoyLife <= medianEL, 0, 1)
-fam2$EnjoyLife<-ifelse(fam2$EnjoyLife <= medianEL, 0, 1)
-# Plot PDF after binarize
-gendist(modfam2, geom_histogram,"EnjoyLife", "bin_modfam2")
-gendist(fam2, geom_histogram,"EnjoyLife", "bin_fam2")
 
-#save binarized fam2,modfam2, merged in HDF5 format
-h5createFile('data/predData.h5')
-# h5write(fam2, "data/predData.h5","fam2")
-# h5write(modfam2,"data/predData.h5","modfam2")
-# h5write(merged, "data/predData.h5", "merged")
 
-# Plot Conditional PDF 
 generateCPDF<-function(somedf, plotfunc, target){
+  # Plot Conditional PDF 
   dfname = deparse(substitute(somedf))
   pfname = deparse(substitute(plotfunc))
   for (i in colnames(somedf)){
@@ -78,10 +102,9 @@ generateCPDF<-function(somedf, plotfunc, target){
     }
   }
 }
-generateCPDF(modfam2,geom_density, "EnjoyLife")
-generateCPDF(fam2, geom_histogram, "EnjoyLife")
 
-fitCPDF<- function(df, xname, yname, f, plotfunc, method){
+
+fitCPDF<- function(df, xname, yname, f, plotfunc, method, start = NULL){
   # Fitting Conditional pdf on top of histogram, and save that plot.
   #
   # Args: 
@@ -98,25 +121,37 @@ fitCPDF<- function(df, xname, yname, f, plotfunc, method){
   pfname = deparse(substitute(plotfunc))
   xcol <- df[[xname]]
   ycol <- df[[yname]]
-  yrange = range(ycol)
+  xrange <- unique(xcol)
+  yrange <- unique(ycol)
+  xlen <- length(xrange)
+  ylen <- length(yrange)
   sf <- list()
-  for (i in range(1,length(yrange))){
+  for (i in range(1,ylen)){
     condData <- xcol[ycol == yrange[i]]
-    params <- c(fitdist(condData, f, method)$estimate)
+    print(condData)
+    params <- c(fitdist(condData, f, method, start = start)$estimate)
     # This step is IMPORTANT
     dfunc = match.fun(paste('d', f, sep = ''))
-    sf[[i]] = stat_function(fun = dfunc, args = params, lty = "dashed")
+    if(pfname == "geom_histogram"){
+      sf[[i]] = stat_function(fun = dfunc, n = xlen, args = params, lty = "dashed")
+    }else{
+      sf[[i]] = stat_function(fun = dfunc, args = params, lty = "dashed")
+    }
   }
   if (pfname == "geom_histogram"){
-    # It is important here that binwidth is 1, so the y is actually density!!!
-#     bw <- 0.01
     ggplot(df) + 
     plotfunc(aes_string(y="..density.. * 0.1", x=xname, group = yname, fill=yname), binwidth=0.1, position="dodge", width = 2) + 
     sf
   } else {
-    ggplot(df) + 
-    plotfunc(aes_string(x=xname, group = yname, group = yname, fill=yname), alpha = .8) + 
-    sf
+    if (f == "beta"){
+      ggplot(df) + 
+        plotfunc(aes_string(x=xname, group = yname, group = yname, fill=yname), alpha = .8) + 
+        sf + scale_x_continuous(limits = c(0,1))
+    } else {
+      ggplot(df) + 
+        plotfunc(aes_string(x=xname, group = yname, group = yname, fill=yname), alpha = .8) + 
+        sf
+    }
   }
  # Save plots
  fpath = paste("plots/",paste(dfname, "fitcpdf", xname,"on",yname, f, sep = "_"), ".pdf", sep="")
@@ -124,37 +159,73 @@ fitCPDF<- function(df, xname, yname, f, plotfunc, method){
 }
 
 
-# List of density functions
-densfunlist <- c("norm", "exp", "gamma", "nbinom", "geom", "beta", "logis", "nbinorm")
-# TODO??
-PickBestFit <- function(x, method){
-  # Fit the best density functions out of densfunlist
-  # Args:
-  #   x: a numeric vector
-  #   method: the character string coding for the fitting method
-  # Returns:
-  #   fitdistr obj with the best performance based on estimated standard error mean
-  errlist <- list()
-  for (f in densfunlist){
-    obj <- fitdist(x, f)
-    stderror <- obj$sd[1]
-    errlist[stderror] <- obj
-  }
-}
+# # List of density functions
+# densfunlist <- c("norm", "exp", "gamma", "nbinom", "geom", "beta", "logis", "nbinorm")
+# # TODO??
+# PickBestFit <- function(x, method){
+#   # Fit the best density functions out of densfunlist
+#   # Args:
+#   #   x: a numeric vector
+#   #   method: the character string coding for the fitting method
+#   # Returns:
+#   #   fitdistr obj with the best performance based on estimated standard error mean
+#   errlist <- list()
+#   for (f in densfunlist){
+#     obj <- fitdist(x, f)
+#     stderror <- obj$sd[1]
+#     errlist[stderror] <- obj
+#   }
+# }
 
 
 ###Examples
+gendist(modfam2, geom_histogram, "EnjoyLife", "modfam2")
+gendist(fam2, geom_histogram, "EnjoyLife", "fam2")
+# Plot PDF after binarize
+gendist(modfam2_bin, geom_histogram,"EnjoyLife", "modfam2_bin")
+gendist(fam2_bin, geom_histogram,"EnjoyLife", "fam2_bin")
+
+gendist(fullTable3, geom_histogram, "ActualEDSS", "fullTable3")
+gendist(fullTable3, geom_density, "ActualEDSS", "fullTable3")
+generateCPDF(modfam2_bin,geom_density, "EnjoyLife")
+generateCPDF(fam2_bin, geom_histogram, "EnjoyLife")
 # Plot Fitted Histogram for fam2 with 'norm' normal distribution
-for (cname in colnames(fam2)){
+for (cname in colnames(fam2_bin)){
   if (cname != "EnjoyLife"){
-    fitCPDF(fam2, cname, "EnjoyLife", "norm", geom_histogram, "mle")
+    fitCPDF(fam2_bin, cname, "EnjoyLife", "norm", geom_histogram, "mle")
   }
 }
 
 # Plot Fitted density for modfam2 with 'norm' normal distribution
-for (cname in colnames(modfam2)){
+for (cname in colnames(modfam2_bin)){
   if (cname != "EnjoyLife"){
     print(cname)
-    fitCPDF(modfam2, cname, "EnjoyLife", "gamma", geom_density, "mme")
+    fitCPDF(modfam2_bin, cname, "EnjoyLife", "gamma", geom_density, "mme")
+  }
+}
+
+
+# Plot Fitted density for modfam2 with 'norm' normal distribution
+for (cname in colnames(modfam2_processing)){
+  if (cname != "EnjoyLife"){
+    print(cname)
+    fitCPDF(modfam2_processing, cname, "EnjoyLife", "beta", geom_density, "mme")
+  }
+}
+
+
+
+
+for (cname in colnames(fam2_bin)){
+  if (cname != "EnjoyLife"){
+    print(cname)
+    fitCPDF(fam2_bin, cname, "EnjoyLife", "pois", geom_histogram, "mme")
+  }
+}
+
+for (cname in colnames(fam2_bin)){
+  if (cname != "EnjoyLife"){
+    print(cname)
+    fitCPDF(fam2_bin, cname, "EnjoyLife", "nbinom", geom_histogram, "mme")
   }
 }
