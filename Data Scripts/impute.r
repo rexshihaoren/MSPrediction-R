4735b0ac558d6fee10adf513adb842af227bf3c6
rexshihaoren <renshihao@gmail.com>
2014-09-04 17:20:32 -0700

    modified liz

--- Data Scripts/impute.r
+++ Data Scripts/impute.r
@@ -9,7 +9,7 @@ file.create(imputePath)
 diagnofinal <- KnnImputeXY(diagnoap, c("ModEDSS"))
 diagno0final <- KnnImputeXY(diagno0ap,c("ModEDSS"))
 ###### If only complete columns #####
-diagnonoNA <- diagnoap[,colSums(is.na(diagnoap))==0]
+diagnonoNA <- diagnoap[,colSums(is.na(diagno0ap))==0]
 ### Save #####
 save(diagnofinal, diagno0final, diagnonoNA, file=imputePath)
 h5write(diagnofinal, filePath,"diagnofinal")
